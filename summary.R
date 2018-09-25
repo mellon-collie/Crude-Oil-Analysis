221cf95cdd684dcb1fe02d6be61cbae896e4b13c
abhishikta-sai <31542711+abhishikta-sai@users.noreply.github.com>
2018-09-25 21:50:02 +0530

    Update summary.R

--- summary.R
+++ summary.R
@@ -1,3 +1,4 @@
 setwd("/home/pruthvi/Desktop/Sem5/Data Analytics/Project/git_stuff/Stock-Analysis")
 dataset <- read.csv("/home/pruthvi/Desktop/Sem5/Data Analytics/Project/final_oil.csv",TRUE,",")
-
+res<-summay(dataset)
+print(res)
