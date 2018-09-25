102e60d79bc6e7d40ff899e506551e771246a2e6
pruthvipatnala <pruthvipatnala@gmail.com>
2018-09-25 21:13:40 +0530

    Changed dataset and added summary file

---
+++ summary.R
@@ -0,0 +1,3 @@
+setwd("/home/pruthvi/Desktop/Sem5/Data Analytics/Project/git_stuff/Stock-Analysis")
+dataset <- read.csv("/home/pruthvi/Desktop/Sem5/Data Analytics/Project/final_oil.csv",TRUE,",")
+
